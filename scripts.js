7cfe5747f1e5a3fedf8f34d0381ea35c4da7241e
jrmelton50 <jrmelton@crimson.ua.edu>
2018-04-09 12:11:37 -0500

    Completed assignment

--- scripts.js
+++ scripts.js
@@ -1,5 +1,15 @@
+var friends = ["Kaitlyn", "Josh", "Alex", "Allison", "Scott"];
+var locations = ["kitchen", "living room", "bathroom", "copy room", "office", "classroom", "sketchy elevator", "stairwell", "drivethru", "driveway"];
+var weapons = ["spatula", "paper clip", "fork", "spoon", "chair", "table", "water bottle", "coffe mug", "tv", "hammer", "trash can", "backpack", "apple", "plastic cup", "car seat", "diaper", "shirt", "sock", "pipe", "phone", ];
+
 for (var i=0; i<100; i++) {
     var $h3 = $("<h3> Accusation " + (i+1) + "</h3>");
-    $h3.onclick
+    $h3.click(displayAlert(i));
     $h3.appendTo("body");
+}
+
+function displayAlert(i) {
+    return function() {
+        alert("Accusation " + (i+1) + ": I accuse " + friends[i % 5] + ", with the " + weapons[i % 20] + " in the "+locations[i % 10] + "!");
+    }
 }
